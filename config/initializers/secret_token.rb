fe418e2783bc7cd366f7799027042abf169f0d8e
eastsideski <eastsideski@gmail.com>
2013-06-27 01:54:41 +0000

    fresh start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wpilife::Application.config.secret_token = '2e64105e84977e7b2c90072695884aaeabc4da0f223f40eb8ab9b6f5ca57af837b1ebbe1b878e7e5620e407bf8f283e240be1b0f88082befbe60831241578bf4'
