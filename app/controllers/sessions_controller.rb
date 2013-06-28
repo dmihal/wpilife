12fd25e1e513c6b5d4283426fa60fd7d3acad121
eastsideski <eastsideski@gmail.com>
2013-06-28 16:31:21 +0000

    Full-featured user system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash[:success] = 'Welcome back!'
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
-    flash[:success] = 'You have been logged out. Come back soon!'
+    flash[:notice] = 'You have been logged out. Come back soon!'
     redirect_to root_url
   end
 end
